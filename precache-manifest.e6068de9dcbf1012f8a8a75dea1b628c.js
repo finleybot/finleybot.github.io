355453cdda289c6988a8c0db9271da4d0d1fe4fa
zamoox <zamoox@gmail.com>
2020-06-05 01:10:49 +0300

    Updates

---
+++ precache-manifest.e6068de9dcbf1012f8a8a75dea1b628c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd1b53e2165a904b202a8953bdf8c7c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c71015d8dfb723d858f",
+    "url": "/static/css/main.2bb53c4d.chunk.css"
+  },
+  {
+    "revision": "74566f47a137278b9058",
+    "url": "/static/js/2.cbda6ba9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cbda6ba9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c71015d8dfb723d858f",
+    "url": "/static/js/main.01922577.chunk.js"
+  },
+  {
+    "revision": "f0ff99ccfcea27993787",
+    "url": "/static/js/runtime-main.6bcb2c22.js"
+  },
+  {
+    "revision": "a5e611269a38c01bf93783fb14230664",
+    "url": "/static/media/background.a5e61126.jpg"
+  },
+  {
+    "revision": "0429ffc6dab856131fb957d83013a995",
+    "url": "/static/media/dima.0429ffc6.jpg"
+  },
+  {
+    "revision": "a9797c7bf489d3be5dd1d0d3f679c944",
+    "url": "/static/media/maks.a9797c7b.jpg"
+  },
+  {
+    "revision": "beff86455aa895d40f3dc91223865b40",
+    "url": "/static/media/vanya.beff8645.jpg"
+  },
+  {
+    "revision": "6074b0cc088635c108496576071d9368",
+    "url": "/static/media/vlad.6074b0cc.jpg"
+  }
+]);
